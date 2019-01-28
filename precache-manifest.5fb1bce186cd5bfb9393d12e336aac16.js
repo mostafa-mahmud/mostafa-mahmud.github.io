344ab8a12e1de4d50614a06003543ecb4f22587b
mostafa-mahmud <45487527+mostafa-mahmud@users.noreply.github.com>
2019-01-28 20:29:49 +0200

    Add files via upload

---
+++ precache-manifest.5fb1bce186cd5bfb9393d12e336aac16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bfdee0c1e34a732f8169",
+    "url": "/static/js/main.bfdee0c1.chunk.js"
+  },
+  {
+    "revision": "468b232bda591aa09ba7",
+    "url": "/static/js/1.468b232b.chunk.js"
+  },
+  {
+    "revision": "bfdee0c1e34a732f8169",
+    "url": "/static/css/main.04822725.chunk.css"
+  },
+  {
+    "revision": "851d18b3e3f70012fe7adfecc99c91b1",
+    "url": "/index.html"
+  }
+];
